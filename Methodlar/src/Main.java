1a5cbaed9e8d9aff23c70b2be78b9a2140168e21
Yusuf Sahin <yusuf@yusufsahin.net>
2023-07-11 11:34:21 +0300

    Methodlar

---
+++ Methodlar/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String isim= scanner.nextLine();
+        System.out.println("Merhaba dünya!,"+isim);
+        selamla();
+        selamla(isim);
+        System.out.println(topla(5,3));
+        System.out.println(selam());
+        System.out.println(selam(isim));
+    }
+    public static  void selamla(){
+        System.out.println("Merhaba Dünya!");
+    }
+    public static  void selamla(String isim){
+        System.out.println("Merhaba dünya!,"+isim);
+    }
+    public static  int topla(int a ,int b){
+        return a+b;
+    }
+    //method adı değişemesi lazım
+    //  public static String selamla()
+    //   {
+    //     return "Merhaba Dünya!";
+    //  }
+    public static  String selam()
+    {
+        return "Merhaba Dünya";
+    }
+
+    public static  String selam(String isim)
+    {
+        return "Merhaba Dünya "+isim;
+    }
+}
