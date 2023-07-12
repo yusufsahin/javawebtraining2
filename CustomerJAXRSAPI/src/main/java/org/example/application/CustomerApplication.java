c1ed247d53600311af5437d01d990548a274e454
Yusuf Sahin <yusuf@yusufsahin.net>
2023-07-12 16:02:37 +0300

    Customer JAX-RS API

---
+++ CustomerJAXRSAPI/src/main/java/org/example/application/CustomerApplication.java
@@ -0,0 +1,19 @@
+package org.example.application;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import org.example.resources.CustomerResources;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class CustomerApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses()
+    {
+        Set<Class<?>> classes= new HashSet<>();
+        classes.add(CustomerResources.class);
+        return  classes;
+    }
+}
